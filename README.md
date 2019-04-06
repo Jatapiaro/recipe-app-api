e7f1c1e9e3eccbde29d09b628156e233fbdd3710
Jacobo Tapia <jacob.et.cetera@gmail.com>
2019-04-06 15:21:24 -0600

    Añade custom user

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 ## Create the django project
 `docker-compose run app sh -c "django-admin.py startproject app ."`
-
+`docker-compose run app sh -c "django-admin.py startproject core"`
 
 # Run the tests
 `docker-compose run app sh -c "python manage.py test && flake8"`
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTest(TestCase):
+
+    def test_create_user_successful(self):
+        """ Test creating a new user with an email is successful """
+        email = "test@test.com"
+        password = "password"
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    
+    def test_new_user_normalize(self):
+        email = "test@TEST.COM"
+        password = "password"
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+        self.assertEqual(user.email, email.lower())
+
+    
+    def test_new_user_invalid_email(self):
+        """ Test creating user with no email raises error """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email = None,
+                password = "123456"
+            )
+    
+
+    def test_create_new_superuser(self):
+        """ Test creating a new super user """
+        user = get_user_model().objects.create_superuser(
+            email = "test@test.com",
+            password = "123456"
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
